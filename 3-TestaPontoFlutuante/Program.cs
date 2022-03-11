e14b4f7f12f7dcdf1da7c42a40a9224649c09ac9
Raphael Carvalho <raphilskerj@gmail.com>
2022-03-11 16:44:35 -0300

    Aula 08 - Conversões e outros tipos numéricos - Variáveis Numéricas

---
+++ 3-TestaPontoFlutuante/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_TestaPontoFlutuante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 3 - Criando variáveis ponto flutuante");
+
+            double salario;
+            salario = 1250.70;
+
+            Console.WriteLine(salario);
+
+            int divisao = 5 / 2;
+            Console.WriteLine(divisao);
+
+            double division = 5.0 / 2;
+            Console.WriteLine(division);
+
+            Console.WriteLine("A execução acabou. Tecle Enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
