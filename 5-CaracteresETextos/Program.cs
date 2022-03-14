736c34b1ab54a23657fbff9ec1f6cb61f7326071
Raphael Carvalho <raphilskerj@gmail.com>
2022-03-14 10:59:08 -0300

    Aula 03 - Cacteres e Textos - Variáveis de Texto

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_CaracteresETextos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 5 - Caracteres e Textos");
+
+            char letra = 'a';
+            Console.WriteLine(letra);
+
+            char valor = (char)65;
+            Console.WriteLine(valor);
+
+            valor = (char)(valor + 1);
+            Console.WriteLine(valor);
+
+            string palavra = "alura cursos online de tecnologia ";
+            Console.WriteLine(palavra);
+
+            palavra = palavra + 2020;
+            Console.WriteLine(palavra);
+
+            Console.ReadLine();
+        }
+    }
+}
