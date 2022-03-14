1773c872bd89cd19716879cfb168100c0e37fdf8
Raphael Carvalho <raphilskerj@gmail.com>
2022-03-14 16:44:38 -0300

    Aula 11 - Escopo Variáveis - Controle de fluxo com IF

---
+++ 10-TestaEscopo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _10_TestaCondicional2
+{
+    class Programa
+    {
+        static void Main( string[ ] args )
+        {
+            Console.WriteLine( "Executando projeto 10 - Testando Escopo" );
+
+            int idade = 20;
+            int quantidadePessoas = 3;
+            bool acompanhado;
+
+            if (quantidadePessoas >= 2)
+            {
+                acompanhado = true;
+            }
+            else
+            {
+                acompanhado = false;
+            }
+
+            if (idade >= 18 || acompanhado)
+            {
+                Console.WriteLine( "Seja bem vindo" );
+            }
+            else
+            {
+                Console.WriteLine( "Infelizmente, você não pode entrar!" );
+            }
+
+            Console.WriteLine( "A execução acabou. Tecle Enter para sair!" );
+        }
+    }
+}
