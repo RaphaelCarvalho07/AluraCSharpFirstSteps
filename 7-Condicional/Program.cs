eed2c1cbe056183f12bd069477341a3563079669
Raphael Carvalho <raphilskerj@gmail.com>
2022-03-14 12:36:26 -0300

    Aula 03 - Exercício  Condicional IF - Controle de fluxo com IF

---
+++ 7-Condicional/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Condicional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 7 - Condicional IF");
+
+            int idade = 20;
+            int qtdPessoas = 3;
+
+            if (idade >= 18)
+            {
+                Console.WriteLine("Você tem mais que 18 anos!");
+                Console.WriteLine("Seja Bem-Vindo!");
+            }
+            else if (qtdPessoas >= 2)
+            {
+                Console.WriteLine("Você não tem 18, mas pode entrar, pois está acompanhado! =P");
+            }
+            else
+            {
+                Console.WriteLine("Infelizmente você não pode entrar =(");
+            }
+
+            Console.WriteLine("A execução acabou. Tecle Enter para sair");
+            Console.ReadLine();
+        }
+    }
+}
