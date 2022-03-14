f3151edf626a63e6cae3a4f3134a33b45b2b12d2
Raphael Carvalho <raphilskerj@gmail.com>
2022-03-14 15:39:04 -0300

    Aula 08 - Um pouco mais de IF - Controle de fluxo com IF

---
+++ 9-TestaCondicional2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _9_TestaCondicional2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 9 - Testando Condicionais 2");
+
+            int idade = 16;
+            // int qtdPessoas = 3;
+            bool acompanhado = true;
+
+            if (idade >= 18 && acompanhado)
+            {
+                Console.WriteLine("Seja bem vindo");
+            }
+            else
+            {
+                Console.WriteLine("Infelizmente, você não pode entrar.");
+            }
+
+            Console.WriteLine("A execução acabou. Tecle Enter para sair!");
+            Console.ReadLine();
+        }
+    }
+}
