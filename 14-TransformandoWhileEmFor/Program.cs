75403e133124f39e4f9f9dd63309e52a7e8725a8
Raphael Carvalho <raphilskerj@gmail.com>
2022-03-15 07:48:19 -0300

    Aula 07 - Transformando While em For - Controle de Fluxo com Laços de Repetição

---
+++ 14-TransformandoWhileEmFor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_TransformandoWhileEmFor
+{
+    internal class Program
+    {
+        static void Main( string[ ] args )
+        {
+            Console.WriteLine( "Executando projeto 14 - Laço de Repetição For - Transformando While em For" );
+
+            /*int contador = 1;
+            while (contador <= 10)
+            {
+                Console.WriteLine( contador );
+                contador++;
+            }*/
+
+            for (int contador = 1; contador <= 10; contador++)
+            {
+                Console.WriteLine( contador );
+            }
+        }
+    }
+}
